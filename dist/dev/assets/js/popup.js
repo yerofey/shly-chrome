0e6d89c1a0a57608ea51dd0e34f49f91d32da83d
Yerofey S <pm@yerofey.dev>
2022-08-09 17:02:58 +0200

    v2.17.1 - fixed URL object bug with Opera browser

--- dist/dev/assets/js/popup.js
+++ dist/dev/assets/js/popup.js
@@ -1,24 +1,24 @@
 // @ts-nocheck
-"use strict";
+'use strict';
 
-const el_error = document.getElementById("error");
-const el_form = document.getElementById("form");
-const el_form_copy = document.getElementById("button");
-const el_form_input = document.getElementById("input");
-const el_loading = document.getElementById("loading");
-const el_qr_a = document.getElementById("qr-link");
-const el_qr_img = document.getElementById("qr-img");
+const el_error = document.getElementById('error');
+const el_form = document.getElementById('form');
+const el_form_copy = document.getElementById('button');
+const el_form_input = document.getElementById('input');
+const el_loading = document.getElementById('loading');
+const el_qr_a = document.getElementById('qr-link');
+const el_qr_img = document.getElementById('qr-img');
 
 const App = {
   init: function () {
     // generate default QR
-    const qr = qrcode(0, "Q");
-    qr.addData("https://shly.link/");
+    const qr = qrcode(0, 'Q');
+    qr.addData('https://shly.link/');
     qr.make();
-    document.getElementById("qr-code").innerHTML = qr.createImgTag(4);
+    document.getElementById('qr-code').innerHTML = qr.createImgTag(4);
 
     // init placeholder
-    el_form_input.setAttribute("placeholder", App.langText("inputPlaceholder"));
+    el_form_input.setAttribute('placeholder', App.langText('inputPlaceholder'));
   },
   inputCopyLink: function () {
     Func.copyToClipboard(el_form_input.value);
@@ -34,26 +34,20 @@ const App = {
   },
   shorten: function (page) {
     let urlObject = {};
-    let urlIsValid = true;
+    let urlIsValid = false;
     try {
       urlObject = new URL(page.url);
+			urlIsValid = (urlObject.protocol === 'http:' || urlObject.protocol === 'https:');
     } catch (e) {
       console.error(e);
-      urlIsValid = false;
     }
 
-    // console.log("link", page.url);
-    // console.log("object", urlObject);
-
-    if (
-      urlIsValid &&
-      ["https:", "http:"].includes(urlObject.protocol) &&
-      !["shly.link", "shly.link"].includes(urlObject.domain)
-    ) {
+    if ((urlIsValid || Func.urlIsValid(page.url)) && (page.url).toLowerCase().indexOf('shly.link') === -1) {
+			// !['shly.link'].includes(urlObject.domain)
       const link = encodeURIComponent(page.url);
       const request = new XMLHttpRequest();
       request.open(
-        "GET",
+        'GET',
         `https://shly.link/_/api/?app=aAMHu&url=${link}`,
         true
       );
@@ -64,13 +58,13 @@ const App = {
           DOM.hide(el_loading);
 
           const result = JSON.parse(request.responseText);
-          if (typeof result.error === "undefined") {
+          if (typeof result.error === 'undefined') {
             el_form_input.value = result.result_url;
 
-            const qr = qrcode(0, "Q");
+            const qr = qrcode(0, 'Q');
             qr.addData(result.result_url);
             qr.make();
-            document.getElementById("qr-code").innerHTML =
+            document.getElementById('qr-code').innerHTML =
               qr.createImgTag(4);
 
             DOM.show(el_form);
@@ -79,7 +73,7 @@ const App = {
           } else {
             DOM.hide(el_loading);
             DOM.show(el_error);
-            el_error.innerHTML = App.langText("extensionError") + ": " + result.description;
+            el_error.innerHTML = App.langText('extensionError') + ': ' + result.description;
           }
         }
       };
@@ -87,23 +81,23 @@ const App = {
     } else {
       DOM.hide(el_loading);
       DOM.show(el_error);
-      el_error.innerHTML = App.langText("extensionError") + ": " + App.langText('extensionErrorParseUrl');
+      el_error.innerHTML = App.langText('extensionError') + ': ' + App.langText('extensionErrorParseUrl');
     }
   },
 };
 
 const DOM = {
   hide: function (element) {
-    if (element.classList.contains("_shown")) {
-      element.classList.remove("_shown");
+    if (element.classList.contains('_shown')) {
+      element.classList.remove('_shown');
     }
-    element.classList.add("_hidden");
+    element.classList.add('_hidden');
   },
   show: function (element) {
-    if (element.classList.contains("_hidden")) {
-      element.classList.remove("_hidden");
+    if (element.classList.contains('_hidden')) {
+      element.classList.remove('_hidden');
     }
-    element.classList.add("_shown");
+    element.classList.add('_shown');
   },
 };
 
@@ -112,15 +106,24 @@ const Func = {
     return string.charAt(0).toUpperCase() + string.slice(1);
   },
   copyToClipboard: function (string) {
-    let el = document.createElement("input");
-    el.style.position = "fixed";
-    el.style.opacity = "0";
+    let el = document.createElement('input');
+    el.style.position = 'fixed';
+    el.style.opacity = '0';
     el.value = string;
     document.body.appendChild(el);
     el.select();
-    document.execCommand("Copy");
+    document.execCommand('Copy');
     document.body.removeChild(el);
   },
+	urlIsValid: function(url) {
+		const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+			'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+		return !!pattern.test(url);
+	}
 };
 
 (async function () {
@@ -130,7 +133,7 @@ const Func = {
 		audible: false
 		autoDiscardable: true
 		discarded: false
-		favIconUrl: "https://developer.chrome.com/images/meta/favicon-32x32.png"
+		favIconUrl: 'https://developer.chrome.com/images/meta/favicon-32x32.png'
 		groupId: -1
 		height: 721
 		highlighted: true
@@ -140,20 +143,20 @@ const Func = {
 		mutedInfo: {muted: false}
 		pinned: false
 		selected: true
-		status: "complete"
-		title: "Migrating to Manifest V3 - Chrome Developers"
-		url: "https://developer.chrome.com/docs/extensions/mv3/intro/mv3-migration/"
+		status: 'complete'
+		title: 'Migrating to Manifest V3 - Chrome Developers'
+		url: 'https://developer.chrome.com/docs/extensions/mv3/intro/mv3-migration/'
 		width: 1440
 		windowId: 18
 	*/
 	const activeTab = (await chrome.tabs.query({ active: true }))[0];
 	App.shorten(activeTab);
 
-  el_form_copy.addEventListener("click", function () {
+  el_form_copy.addEventListener('click', function () {
     App.inputCopyLink();
   });
 
-  el_form_input.addEventListener("focus", function () {
+  el_form_input.addEventListener('focus', function () {
     App.inputSelectLink();
   });
 })();
